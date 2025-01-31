e968bd6db72b15788a9eaa054db91fbac7ca5cdc
Priyatosh Sarkar <86162258+priyatoshsarkar@users.noreply.github.com>
2025-01-31 13:06:44 +0000

    datatypes

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,14 @@
+const accountId = 144553
+let accountEmail = "Priyatosh@gmail.com"
+var accountPassword = "12345"
+accountCity = "Kolkata"
+let accountState ;
+//accountId = 2 //not allowed
+accountEmail = "Sathi@gmail.com"
+accountPassword = "6060"
+accountCity = "Sector V"
+
+console.log(accountId)
+console.log(accountEmail)
+
+console.table([accountId , accountEmail , accountPassword , accountCity , accountState])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,10 @@
+"use strict"; // treat all js code as newer version
+//alert( 3 + 3) // we are using node js, not browser
+
+console.log(3+3); console.log("Rajib")
+
+let name = "Sathi"
+let a = 10
+console.log(typeof "Sathi");
+console.log(typeof null);
+
