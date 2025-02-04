eaa1ef906a9b2ba29a500248fe98952f6bf93a62
Priyatosh Sarkar <86162258+priyatoshsarkar@users.noreply.github.com>
2025-02-04 11:37:22 +0000

    object

---
+++ 02_basic/05_Object.js
@@ -0,0 +1,10 @@
+const course ={
+    coursename : "Java",
+    price : "999",
+    courseInstructor : "Rajib kumar"
+}
+//course.courseInstructor
+const {courseInstructor: instructoe} = course
+//console.log(courseInstructor);
+console.log(instructoe);
+
